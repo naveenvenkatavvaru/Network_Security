d9fb7e953b96d42ac7d35201af66618ae33974ec
naveenvenkatavvaru <naveenvenkatavvaru@gmail.com>
2025-04-19 17:17:02 +0530

    logging exception

--- get_data.py
+++ get_data.py
@@ -4,11 +4,13 @@ import json
 
 from dotenv import load_dotenv
 load_dotenv()
-
+MONGO_DB_URL=os.getenv("MONGO_DB_URL")
+print(MONGO_DB_URL)
 
 
 
 import certifi
+ca=certifi.where()
 
 import pandas as pd
 import numpy as np
